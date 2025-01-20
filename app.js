da76633050c73cf0c66abdd34e1b5d32e85e746c
yoelcolque <aquispec@fi.uba.ar>
2025-01-20 16:54:49 -0300

    v1.0.0
    Implementation initial.

---
+++ app.js
@@ -0,0 +1,9 @@
+let titulo = document.querySelector("h1");
+titulo.textContent = "HOLA BIENVENIDO AL JUEGO"
+
+let textCampoGet = document.querySelector("p");
+textCampoGet.textContent = "Ingrese el numero";
+
+function intentoUsuario(){
+    alert("Se realizo el click");
+}
